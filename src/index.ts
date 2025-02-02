06a48163d4c601cc9d4d92e42556aa2a339637f7
Nilesh Ramteke <nileshramteke58@gmail.com>
2025-02-02 20:56:48 +0530

    single and multiple numbers sum implemented

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 
-function add(numberList:string): number {
+function add(numberListStr:string): number {
     let sum: number = 0;
-    if (numberList.trim().length === 0) {
+    if (numberListStr.trim().length === 0) {
         sum = 0;
+    } else {
+        const numbers = numberListStr.split(/[,]/);
+        sum = numbers
+                .map(num => {
+                    const parsed = Number(num.trim());
+                    if (isNaN(parsed)) {
+                        throw new Error(`Invalid number: ${num}`);
+                    }
+                    return parsed;
+                })
+                .reduce((sum, num) => sum + num, 0);
     }
     return sum;
 }
