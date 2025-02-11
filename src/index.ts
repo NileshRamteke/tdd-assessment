cc2f4bfaf48e3f7188d4205e6d2abc4cfab41706
Nilesh Ramteke <nileshramteke58@gmail.com>
2025-02-11 15:41:29 +0530

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,74 @@
 function extractDelimitors(numberListStr: string): string {
-    const startIndex = numberListStr.indexOf("//") + 2;
-    const endIndex = numberListStr.indexOf("\n", startIndex);   
-    const delim = numberListStr.substring(startIndex, endIndex);
-    return delim;
+  const startIndex = numberListStr.indexOf("//") + 2;
+  const endIndex = numberListStr.indexOf("\n", startIndex);
+  const delim = numberListStr.substring(startIndex, endIndex);
+  return delim;
 }
 
-function extractMultipleDelimitors(delimitorStr:string) {
-    const matches = delimitorStr.match(/\[(.*?)\]/g) || [];
-    return matches.map((match:string) => match.slice(1, -1));
+function extractMultipleDelimitors(delimitorStr: string) {
+  const matches = delimitorStr.match(/\[(.*?)\]/g) || [];
+  return matches.map((match: string) => match.slice(1, -1));
 }
-function getNumbersFromChangedDelimitor(numberListStr: string):string[] {
-    let numbers: string[] = [];
-    const delim: string = extractDelimitors(numberListStr);
-    const isMultipleDelims:boolean = delim.indexOf("[") > -1 && delim.indexOf("]") > -1;
-    const numbersStr:string = numberListStr.split("\n")[1];
-    if (isMultipleDelims) {
-        const multipleDelimitors:string[] = extractMultipleDelimitors(delim);
-        const escapedDelims:string[] = multipleDelimitors.map(delim => delim.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));
-        const regex:RegExp = new RegExp(escapedDelims.join('|'));
-        numbers = numbersStr.split(regex);
-    } else {
-        numbers = numbersStr.split(delim)
-    }
-    return numbers;
+function getNumbersFromChangedDelimitor(numberListStr: string): string[] {
+  let numbers: string[] = [];
+  const delim: string = extractDelimitors(numberListStr);
+  const isMultipleDelims: boolean =
+    delim.indexOf("[") > -1 && delim.indexOf("]") > -1;
+  const numbersStr: string = numberListStr.split("\n")[1];
+  if (isMultipleDelims) {
+    const multipleDelimitors: string[] = extractMultipleDelimitors(delim);
+    const escapedDelims: string[] = multipleDelimitors.map((delim) =>
+      delim.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
+    );
+    const regex: RegExp = new RegExp(escapedDelims.join("|"));
+    numbers = numbersStr.split(regex);
+  } else {
+    numbers = numbersStr.split(delim);
+  }
+  return numbers;
 }
 
 function calculateSum(numbers: string[]) {
-    let sum: number = 
-        numbers
-            .map((num:string) => {
-                let parsed = Number(num.trim());
-                if (isNaN(parsed)) {
-                    throw new Error(`Invalid number: ${num}`);
-                }
-                if (parsed < 0) {
-                    throw new Error("Negative numbers not allowed")
-                }
-                if (parsed > 1000) {
-                    parsed = 0;
-                }
-                return parsed;
-            })
-            .reduce((sum, num) => sum + num, 0);
-    return sum;
+  let sum: number = numbers
+    .map((num: string) => {
+      let parsed = Number(num.trim());
+      if (parsed > 1000) {
+        parsed = 0;
+      }
+      return parsed;
+    })
+    .reduce((sum, num) => sum + num, 0);
+  return sum;
 }
 
-function add(numberListStr:string): number {
-    let sum: number = 0;
-    let isDelimitorChanged:boolean = numberListStr.includes("//");
-    if (numberListStr.trim().length === 0) {
-        sum = 0;
-    } else if(isDelimitorChanged) {
-        const numbers: string[] = getNumbersFromChangedDelimitor(numberListStr);
-        sum = calculateSum(numbers);
-    } else {
-        const numbers = numberListStr.split(/[,\n]/);
-        sum = calculateSum(numbers);
+function checkIfNegatives(numbers: string[]) {
+  const negatives = numbers.filter((num) => {
+    let parsed = Number(num);
+    return parsed < 0;
+  });
+  return negatives.length > 0;
+}
+
+function add(numberListStr: string): number {
+  function checkNegativesAndGetSum(numbers: string[]) {
+    const checkNegatives = checkIfNegatives(numbers);
+    if (checkNegatives) {
+      throw new Error("Negative numbers not allowed");
     }
-    return sum;
+    return calculateSum(numbers);
+  }
+  let sum: number = 0;
+  let isDelimitorChanged: boolean = numberListStr.includes("//");
+  if (numberListStr.trim().length === 0) {
+    sum = 0;
+  } else if (isDelimitorChanged) {
+    const numbers: string[] = getNumbersFromChangedDelimitor(numberListStr);
+    sum = checkNegativesAndGetSum(numbers);
+  } else {
+    const numbers = numberListStr.split(/[,\n]/);
+    sum = checkNegativesAndGetSum(numbers);
+  }
+  return sum;
 }
 
 module.exports = add;
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,41 +1,41 @@
-import {describe, expect, test} from '@jest/globals';
+import { describe, expect, test } from "@jest/globals";
 const add = require("../src/index.ts");
-describe('Add function module', () => {
-    test('check for empty string sum to be 0', () => { 
-        const sum: number = add("");
-        expect(sum).toBe(0);
-    });
-    test('check for single number sum', () => {
-        const sum: number = add("2");
-        expect(sum).toBe(2)
-    });
-    test('check for multiple numbers sum', () => {
-        const sum: number = add("2,3,7");
-        expect(sum).toBe(12)
-    });
-    test('check for newline characters to be allowed as delimitters in numbers list string', () => {
-        const sum: number = add("1\n2,7\n5");
-        expect(sum).toBe(15)
-    });
-    test('check for negative numbers not allowed', () => {    
-        expect(() => {
-            add("1\n2,7\n-5")
-        }).toThrow("Negative numbers not allowed")
-    });
-    test('test delimiter change syntax //[delimiter]\n[numbers…]: 1', () => {
-        const sum: number = add("//;\n1;2;3");
-        expect(sum).toBe(6);
-    });  
-    test('test delimiter change syntax //[delimiter]\n[numbers…]: 2', () => {
-        const sum: number = add("//**\n1**2**3");
-        expect(sum).toBe(6);
-    });    
-    test("test delimiter change syntax //[delimiter1][delimiter2]...\n[numbers…]: 3", () => {
-            const sum: number = add("//[##][%]\n1##2%3");
-            expect(sum).toBe(6);
-    });
-    test("test 1 ignore numbers greater than 1000: 1", () => {
-        const sum: number = add("//;\n1;2;3;3000");
-        expect(sum).toBe(6);
-});
+describe("Add function module", () => {
+  test("check for empty string sum to be 0", () => {
+    const sum: number = add("");
+    expect(sum).toBe(0);
+  });
+  test("check for single number sum", () => {
+    const sum: number = add("2");
+    expect(sum).toBe(2);
+  });
+  test("check for multiple numbers sum", () => {
+    const sum: number = add("2,3,7");
+    expect(sum).toBe(12);
+  });
+  test("check for newline characters to be allowed as delimitters in numbers list string", () => {
+    const sum: number = add("1\n2,7\n5");
+    expect(sum).toBe(15);
+  });
+  test("check for negative numbers not allowed", () => {
+    expect(() => {
+      add("1\n2,7\n-5");
+    }).toThrow("Negative numbers not allowed");
+  });
+  test("test delimiter change syntax //[delimiter]\n[numbers…]: 1", () => {
+    const sum: number = add("//;\n1;2;3");
+    expect(sum).toBe(6);
+  });
+  test("test delimiter change syntax //[delimiter]\n[numbers…]: 2", () => {
+    const sum: number = add("//**\n1**2**3");
+    expect(sum).toBe(6);
+  });
+  test("test delimiter change syntax //[delimiter1][delimiter2]...\n[numbers…]: 3", () => {
+    const sum: number = add("//[##][%]\n1##2%3");
+    expect(sum).toBe(6);
+  });
+  test("test 1 ignore numbers greater than 1000: 1", () => {
+    const sum: number = add("//;\n1;2;3;3000");
+    expect(sum).toBe(6);
+  });
 });
